d55236db3713c174789046be5557bfc5368aa708
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-01-29 11:03:47 -0500

    update src js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const postDb = async (content) => {
-  console.log('Post to the ase');
+  console.log('Post to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -23,9 +23,9 @@ export const postDb = async (content) => {
   console.log('data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getAllDb = async () => {
-  console.log('GET all from the database');
+// TODO: Add logic for a method that gets to the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
@@ -35,6 +35,7 @@ export const getAllDb = async () => {
   return result;
 };
 
+//Get one from the database
 export const getOneDb = async (id) => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
@@ -45,6 +46,7 @@ export const getOneDb = async (id) => {
   console.log('result.value', result);
   return result;
 };
+//Use to DELETE to the database.
 export const deleteDb = async (id) => {
   console.log('DELETE from the database', id);
   const jateDb = await openDB('jate', 1);
@@ -53,9 +55,10 @@ export const deleteDb = async (id) => {
   const request = store.delete(id);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
+//add to the database
 export const putDb = async (id, content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
@@ -64,7 +67,6 @@ export const putDb = async (id, content) => {
   const request = store.put({ id: id, jate: content });
   const result = await request;
   console.log('data saved to the database', result);
-  return result;
 };
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,33 +1,36 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Added an event handler to the `beforeinstallprompt` event
+// Added an event handler to the `beforeinstallprompt` event
+
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
+
+    // Store the triggered events
     window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
   });
 
-// TODO: Implemented a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
   
-    const promptEvent = window.deferredPrompt;
-  
-    if (!promptEvent) {
-     return;
-    }
-  
-    promptEvent.prompt();
-    
-    const result = await promptEvent.userChoice;
-    
-    window.deferredPrompt = null;
-    
-    butInstall.classList.toggle('hidden', false);
-  });
+  const promptEvent = window.deferredPrompt;
 
-// TODO: Added an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
+  if (!promptEvent) {
+   return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
   
-    window.deferredPrompt = null;
-  }); 
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 
+
